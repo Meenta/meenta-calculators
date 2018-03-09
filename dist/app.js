44fd8bcb2d50017314086a2d7df33d4d7c5e79c8
Stephan Smith <stephan.smith.bc93@gmail.com>
2018-03-09 11:50:49 -0500

    Added in a responsive table fix

--- dist/app.js
+++ dist/app.js
@@ -78973,49 +78973,51 @@ app.directive('results', function() {
 		}],
     template: `
     <div ng-if="results && results.length > 0">
-      <table class="table table-striped">
-        <tr>
-          <th width="15%" align="center">Instrument</th>
-          <th width="15%" align="center">Mode</th>
-          <th width="15%" align="center" ng-if="parameters.settings.showIlluminaSpecifications">Ill Reads</th>
-          <th width="15%" align="center" ng-if="parameters.settings.showIlluminaSpecifications">Ill. Output</th>
-          <th width="15%" align="center" ng-if="parameters.settings.showIlluminaSpecifications">Predicted clusters per library (M)</th>
-          <th width="15%" style="text-align: center" align="center">Clusters</th>
-          <th width="15%" style="text-align: center" align="center">Output</th>
-          <th width="15%" style="text-align: center" align="center">Clusters per Library</th>
-          <th width="15%" style="text-align: center" align="center">
-            Effective Coverage
-          </th>
-          <th>
-            &nbsp;
-          </th>
-        </tr>
-        <tr ng-if="$index < allowedResults" ng-repeat="i in results | orderBy: '+output.coveragePerGenome'">
-          <td>
-            {{ i.data.instrument | instrumentName }}
-          </td>
-          <td>
-            {{ i.data.mode | modeName }}
-          </td>
-          <td align="center" title="Reads: {{ i.data.reads }}">
-            {{ i.data.reads }}
-          </td>
-          <td align="center">
-            {{ i.output.actualOutput | number: 2 }} Gb
-          </td>
-          <td align="center">
-            {{ i.output.predReads * 1000 | number: 2 }} M
-          </td>
-          <td align="center">
-            {{ i.output.coveragePerGenome | number: 0 }}x
-          </td>
-          <td align="center">
-            <button class="btn btn-outline-secondary" ng-click="resultDetail(i)">
-              <i class="fa fa-info-circle"></i>
-            </button>
-          </td>
-        </tr>
-      </table>
+      <div class="table-responsive-sm">
+        <table class="table table-striped table-responsive">
+          <tr>
+            <th width="15%" align="center">Instrument</th>
+            <th width="15%" align="center">Mode</th>
+            <th width="15%" align="center" ng-if="parameters.settings.showIlluminaSpecifications">Ill Reads</th>
+            <th width="15%" align="center" ng-if="parameters.settings.showIlluminaSpecifications">Ill. Output</th>
+            <th width="15%" align="center" ng-if="parameters.settings.showIlluminaSpecifications">Predicted clusters per library (M)</th>
+            <th width="15%" style="text-align: center" align="center">Clusters</th>
+            <th width="15%" style="text-align: center" align="center">Output</th>
+            <th width="15%" style="text-align: center" align="center">Clusters per Library</th>
+            <th width="15%" style="text-align: center" align="center">
+              Effective Coverage
+            </th>
+            <th>
+              &nbsp;
+            </th>
+          </tr>
+          <tr ng-if="$index < allowedResults" ng-repeat="i in results | orderBy: '+output.coveragePerGenome'">
+            <td>
+              {{ i.data.instrument | instrumentName }}
+            </td>
+            <td>
+              {{ i.data.mode | modeName }}
+            </td>
+            <td align="center" title="Reads: {{ i.data.reads }}">
+              {{ i.data.reads }}
+            </td>
+            <td align="center">
+              {{ i.output.actualOutput | number: 2 }} Gb
+            </td>
+            <td align="center">
+              {{ i.output.predReads * 1000 | number: 2 }} M
+            </td>
+            <td align="center">
+              {{ i.output.coveragePerGenome | number: 0 }}x
+            </td>
+            <td align="center">
+              <button class="btn btn-outline-secondary" ng-click="resultDetail(i)">
+                <i class="fa fa-info-circle"></i>
+              </button>
+            </td>
+          </tr>
+        </table>
+      </div>
 
       <div ng-if="isLoggedIn == true" class="alert alert-success">
          <b>Welcome Back, {{ userInfo.displayName}}</b>
@@ -79030,8 +79032,8 @@ app.directive('results', function() {
          <br>
          <h4 class="text-center">
          <a ng-click="login()"><u>Sign-in to your account</u></a>
-         or
-         <a ng-click="login()"><u>Join our Newsletter</u></a>
+         <!--or
+         <a ng-click="login()"><u>Join our Newsletter</u></a> -->
          </h4>
       </div>
 
